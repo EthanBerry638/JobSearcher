ae9b49066453f099221b75e4d9f6ff6faa0de7ef
Ethan Berry <ethanberry670@gmail.com>
2025-09-05 20:54:35 +0100

    Added multijobprovider

---
+++ JobFetcher/MulitJobProvider.cs
@@ -0,0 +1,29 @@
+using JobFetcherManager;
+using JobListingManager;
+
+namespace Providers
+{
+    public class MultiJobProvider : IJobProvider
+    {
+        private readonly List<IJobProvider> _providers;
+
+        public MultiJobProvider(params IJobProvider[] providers)
+        {
+            _providers = providers.ToList();
+        }
+
+        public async Task<List<JobListing>> GetJobsAsync(int pageNumber)
+        {
+            var allJobs = new List<JobListing>();
+
+            foreach (var provider in _providers)
+            {
+                var jobs = await provider.GetJobsAsync(pageNumber);
+                allJobs.AddRange(jobs);
+            }
+
+            return allJobs;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿using JobFetcherManager;
 using Menus;
+using Providers;
 
 class Program
 {
     static async Task Main(string[] args)
     {
         System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-        
-        var provider = new AdzunaJobProvider();
+
+        var adzuna = new AdzunaJobProvider();
+        var remotive = new RemotiveJobProvider();
+        var provider = new MultiJobProvider(adzuna, remotive);
+
         var menu = new MenuManager(provider);
         await menu.LoopAsync();
     }
